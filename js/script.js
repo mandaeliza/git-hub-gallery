e3e82ab3f102f2b7ae5c6c0cb5fa04a23c05b689
Mandy M. <127141046+mandaeliza@users.noreply.github.com>
2024-01-19 17:30:22 +0100

    Fetched github user info and changed icon

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Global variables
+const profileInfo = document.querySelector(".overview");
+const username = "mandaeliza";
+
+const getProfile = async function () {
+  const profileRequest = await fetch(
+    `https://api.github.com/users/${username}`
+  );
+  const data = await profileRequest.json();
+//   console.log(data);
+  displayProfile(data);
+};
+
+getProfile();
+
+const displayProfile = function (data) {
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  div.innerHTML = `
+    <figure>
+        <img alt = "user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+        <p><strong>Name:</strong> ${data.name}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p> 
+        <p><strong>Location:</strong> ${data.location}</p> 
+        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>`;
+  profileInfo.append(div);
+};
+
